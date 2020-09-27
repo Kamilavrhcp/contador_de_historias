aa3a4e70ef056162213419e4fbbfaa91b2ef03be
Kamila <kamilavrhcp@hotmail.com>
2020-09-26 22:22:34 -0300

    alterações

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function carregar (){
 
   msg.innerHTML = `Agora são ${data.getHours()} hora`
   if (data.getHours() >= 0 && data.getHours() <12) {
-      Bom dia! 
+      "Bom dia!" 
       img.src = 'alessandra.png'
       document.body.style.background = "#734023"
   } else if (data.getHours() >= 12 && data.getHours() < 18){
-      Boa tarde!
+      "Boa tarde!"
       img.src = 'matteo2_n.jpg'
       document.body.style.background = "#58656c"
   } else {
